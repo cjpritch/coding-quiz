9acdf22e2135a41e6eb376dabd4aeb932322a1a3
Carly Pritchard <cpritch616@gmail.com>
2022-09-03 14:34:48 -0500

    html for landing page and set up questions array

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+const questions = [
+    {
+        question: "TEST QUESTION 1?",
+        choices: ["A", "C", "C", "D"],
+        answer: "A"
+    },
+    {
+        question: "TEST QUESTION 2?",
+        choices: ["A", "C", "C", "D"],
+        answer: "A"
+    },
+    {
+        question: "TEST QUESTION 3?",
+        choices: ["A", "C", "C", "D"],
+        answer: "A"
+    },
+    {
+        question: "TEST QUESTION 4?",
+        choices: ["A", "C", "C", "D"],
+        answer: "A"
+    },
+    {
+        question: "TEST QUESTION 5?",
+        choices: ["A", "C", "C", "D"],
+        answer: "A"
+    },
+    {
+        question: "TEST QUESTION 6?",
+        choices: ["A", "C", "C", "D"],
+        answer: "A"
+    },
+    {
+        question: "TEST QUESTION 7?",
+        choices: ["A", "C", "C", "D"],
+        answer: "A"
+    },
+    {
+        question: "TEST QUESTION 8?",
+        choices: ["A", "C", "C", "D"],
+        answer: "A"
+    },
+    {
+        question: "TEST QUESTION 9?",
+        choices: ["A", "C", "C", "D"],
+        answer: "A"
+    },
+    {
+        question: "TEST QUESTION 10?",
+        choices: ["A", "C", "C", "D"],
+        answer: "A"
+    },
+];
