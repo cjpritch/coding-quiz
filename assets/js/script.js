e987d08c0dc4366478c0e00eff1421b9266b2313
Carly Pritchard <cpritch616@gmail.com>
2022-09-03 14:50:17 -0500

    add question display html and function to start new quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+var totalTime = 75;
+var timer = document.getElementById("timer");
+var timeLeft = document.getElementById("timeLeft");
+var timesUp = document.getElementById("timesUp");
+var startDiv = document.getElementById("start");
+var startBtn = document.getElementById("startBtn");
+var questionDiv = document.getElementById("questionDiv");
+var question = document.getElementById("question");
+var choiceA = document.getElementById("btn1");
+var choiceB = document.getElementById("btn2");
+var choiceC = document.getElementById("btn3");
+var choiceD = document.getElementById("btn4");
+var answerCheck = document.getElementById("checkAnswer");
+
+// questions the quiz will pull from
 const questions = [
     {
         question: "TEST QUESTION 1?",
@@ -50,3 +65,32 @@ const questions = [
         answer: "A"
     },
 ];
+
+function newQuiz() {
+    questionIndex = 0;
+    totalTime = 75;
+    timeLeft.textContent = totalTime;
+    initialInput.textContent = "";
+
+    startDiv.style.display = "none";
+    questionDiv.style.display = "block";
+    timer.style.display = "block";
+    timesUp.style.display = "none";
+
+    var startTimer = setInterval(function() {
+        totalTime--;
+        timeLeft.textContent = totalTime;
+        if(totalTime <= 0) {
+            clearInterval(startTimer);
+            if (questionIndex < questions.length - 1) {
+                gameOver();
+            }
+        }
+    },1000);
+
+    showQuiz();
+};
+
+
+
+startBtn.addEventListener("click", newQuiz);
